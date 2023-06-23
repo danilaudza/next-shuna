655a18d4a39e9c7da709fc0c9b491bebd11e2319
Dani Laudza <danilaudza@gmail.com>
2023-06-23 20:49:19 +0700

    [REF] frontend adjustment

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,58 +1,114 @@
-"use client";
+"use client"
 
-import Link from "next/link";
-import * as z from "zod";
-import { Input } from "@/components/ui/input";
-import { useChat } from "ai/react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image"
+import { useChat } from "ai/react"
 
-import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
-const ChatPage = () => {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+const knownLanguages = ["javascript", "python", "java", "html"]
+
+const Chat = () => {
+  const { messages, input, handleInputChange, handleSubmit } = useChat()
+
+  function messageContainsCodeBlock(text: any) {
+    return text.includes("```")
+  }
+
+  function renderMessageWithCodeBlock(text: any) {
+    const parts = text.split("```")
+    const renderedParts = parts.map((part: any, index: any) => {
+      if (index % 2 === 1) {
+        const [language, ...codeLines] = part.trim().split("\n")
+        const normalizedLanguage = knownLanguages.includes(
+          language.toLowerCase()
+        )
+          ? language.trim()
+          : "text"
+        const code = codeLines.join("\n")
+        return (
+          <pre className="relative my-2 rounded-lg bg-muted p-4">
+            <p className="absolute inset-x-0 top-0 m-0 rounded-t bg-primary px-4 py-1 text-sm text-primary-foreground">
+              {normalizedLanguage}
+            </p>
+            <div className="mt-4">
+              <code className="font-mono text-sm">{code}</code>
+            </div>
+          </pre>
+        )
+      }
+      //
+      return <p>{part}</p>
+    })
+    return renderedParts
+  }
 
   return (
-    <>
-      <div className="relative h-screen">
-        <div className="rel mx-auto w-full max-w-md py-24 flex flex-col">
-          {messages.map((m) => (
-            <div key={m.id} className="grid space-y-5">
-              {m.role === "user" ? (
-                <div className="my-2 justify-self-end">
-                  <p className="text-sm text-muted-foreground">You</p>
-                  <div className="border text-right rounded-lg p-3 ">
-                    {m.content}
-                  </div>
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="mb-24 flex flex-col items-center justify-center gap-2">
+        {/* <div className="w-full flex flex-col"> */}
+        <Image
+          src="/assets/shuna-trans.png"
+          alt=""
+          width={150}
+          height={150}
+          className="rounded-full"
+          priority={true}
+        />
+        <p>Wanna chat?</p>
+        {messages.map((m) => (
+          <div key={m.id} className="w-full max-w-3xl">
+            {m.role === "user" ? (
+              <div className="flex flex-col items-end justify-end">
+                <p className="text-muted-foreground">You</p>
+                <p className="h-full rounded-md border border-input bg-transparent px-3 py-2 text-sm leading-7 ring-offset-background ">
+                  {m.content}
+                </p>
+              </div>
+            ) : (
+              <div className="flex flex-col items-start justify-start">
+                <div className="mb-2 flex items-center gap-2">
+                  <Avatar>
+                    <AvatarImage src="/assets/shuna.png" />
+                    <AvatarFallback>SN</AvatarFallback>
+                  </Avatar>
+                  <p className="text-muted-foreground">Shuna</p>
                 </div>
-              ) : (
-                <div className="my-2 justify-self-start">
-                  <div className="flex flex-row items-center mb-2">
-                    <Avatar>
-                      <AvatarImage src="/shuna.png" />
-                      <AvatarFallback>SN</AvatarFallback>
-                    </Avatar>
-                    <p className="text-sm text-muted-foreground">Shuna</p>
-                  </div>
-                  <div className="border text-right rounded-lg p-3 ">
-                    {m.content}
-                  </div>
+                <div>
+                  {messageContainsCodeBlock(m.content) ? (
+                    <div className="h-full rounded-md border border-input bg-transparent px-3 py-2 text-sm leading-7 ring-offset-background ">
+                      {renderMessageWithCodeBlock(m.content)}
+                    </div>
+                  ) : (
+                    <div>
+                      <p className="h-full rounded-md border border-input bg-transparent px-3 py-2 text-sm leading-7 ring-offset-background ">
+                        {m.content}
+                      </p>
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-          ))}
-        </div>
-
-        <div className="absolute bottom-0 bg-muted py-6 px-6 inset-x-0">
-          <form onSubmit={handleSubmit} className="flex flex-row space-x-2">
-            <Input className="" value={input} onChange={handleInputChange} />
-            <Button variant="default" type="submit">
-              Send
-            </Button>
+              </div>
+            )}
+          </div>
+        ))}
+        <div className="fixed inset-x-0 bottom-0 bg-primary-foreground to-white pb-10 pt-4">
+          <form
+            onSubmit={handleSubmit}
+            className="mx-auto flex max-w-lg items-center gap-2"
+          >
+            <Input
+              value={input}
+              placeholder="Say Something.."
+              onChange={handleInputChange}
+            />
+            <Button type="submit">Send</Button>
           </form>
+          {/* </div> */}
         </div>
       </div>
-    </>
-  );
-};
+    </section>
+  )
+}
 
-export default ChatPage;
+export default Chat
